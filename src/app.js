c421da843a78c0006cc16519d45d0dbc152ea8f7
yologger <yologger1013@gmail.com>
2020-07-14 22:37:59 +0900

    Thrid commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
 
 // router
 const userRouter = require('./route/user')
@@ -10,6 +11,6 @@ app.use('/test', (req, res) => {
 
 app.use('/user', userRouter)
 
-app.listen(8000, () => {
-    console.log('Server running on 8000 port.')
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
 })
