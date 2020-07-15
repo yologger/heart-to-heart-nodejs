19d0b7c1a5f5adaeca74f6ba826b3946e9afc5f8
yologger <yologger1013@gmail.com>
2020-07-15 15:47:54 +0900

    Add morgan, body-parser, cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
 const port = process.env.PORT || 3000;
 
-// router
+app.use('/', morgan('dev'))
+app.use('/', bodyParser.urlencoded({ extended: false }))
+app.use('/', bodyParser.json())
+app.use('/', cookieParser())
+
+// routers
 const userRouter = require('./route/user')
 
 app.use('/test', (req, res) => {
